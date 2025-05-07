f92ea58301230633d6827a6a0983e18fb0ab5157
Juan Andrés <juanandresrn.2005@gmail.com>
2025-05-06 19:48:38 -0500

    🌱 Commit - Add table patients

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +18,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+
+    # Unprotect all models
     public function boot(): void
     {
-        //
+        Model::unguard();
     }
 }
